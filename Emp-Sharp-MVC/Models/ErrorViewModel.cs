bd968629eb1e78f5397d9bef8450b0560d3abeb1
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-08-25 14:49:18 +0200

    initial commit

---
+++ Emp-Sharp-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Emp_Sharp_MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
