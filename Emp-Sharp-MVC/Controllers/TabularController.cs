06a23af2d5e660bf3be0e1872679b2ec2826eeca
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-08-29 18:44:09 +0200

    new Controller-View

---
+++ Emp-Sharp-MVC/Controllers/TabularController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Emp_Sharp_MVC.Controllers;
+
+public class TabularController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
